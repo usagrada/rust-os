f1cbf28a166223516f4c74c12292904ca6531c7e
usagrada <usagrada.kind@gmail.com>
2022-01-20 01:02:10 +0900

    テスト

--- tests/basic_root.rs
+++ tests/basic_root.rs
@@ -4,28 +4,15 @@
 #![test_runner(blog_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-use blog_os::println;
 use core::panic::PanicInfo;
 
-#[no_mangle]
+#[no_mangle] // この関数の名前を変えない
 pub extern "C" fn _start() -> ! {
-  println!("Hello {}", "World!");
-  // panic!("Some panic message");
-
-  #[cfg(test)]
   test_main();
 
   loop {}
 }
 
-#[cfg(not(test))] // 新しく追加した属性
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-  println!("{}", info);
-  loop {}
-}
-
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
   blog_os::test_panic_handler(info)
